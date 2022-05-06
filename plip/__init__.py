feba8090a13d956fff91bcb656ca1bfdd574cb15
Andrea <andrea.karlova@gmail.com>
2022-05-06 15:09:22 +0100

    fixing the dependencies in the package

--- plip/__init__.py
+++
@@ -1,4 +0,0 @@
-from plip.basic import *
-from plip.visualization import *
-from plip.exchange import *
-from plip.structure import *
--- setup.py
+++ setup.py
@@ -1,12 +1,9 @@
 from setuptools import setup, find_packages
 
-from plip.basic import config
-
 setup(name='plip',
-      version=config.__version__,
+      version=1.0.0,
       description='fully automated protein-ligand interaction profiler',
       packages=find_packages(),
-#      scripts=['plip/plipcmd.py'],
       install_requires=[
           'openbabel',
           'numpy',
